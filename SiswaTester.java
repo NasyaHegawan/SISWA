151ee91341597eaef19b087421e65756c2a067ae
NasyaHegawan <nasya_putri_33rpl@student.smktelkom-mlg.sch.id>
2024-08-19 19:23:11 +0700

    Add files via upload

--- SiswaTester.java
+++ SiswaTester.java
@@ -4,12 +4,31 @@ public class SiswaTester {
         //membuat Object
         //namaClass namaObject = new Constructor ();
         Siswa nasya = new Siswa();
-        Siswa natanz = new Siswa();
+        Siswa zahra = new Siswa();
+        Siswa cinta = new Siswa();
+        Siswa grace = new Siswa();
+        Siswa lia = new Siswa();
 
         nasya.id = 26;
         nasya.nama = "Nasya";
-        natanz.id = 27;
-        natanz.nama = "Natanz";
+        nasya.ipk = 99.8;
+        zahra.id = 35;
+        zahra.nama = "Zahra";
+        zahra.ipk = 90.3;
+        cinta.id = 28;
+        cinta.nama = "Cinta";
+        cinta.ipk = 91.9;
+        grace.id = 12;
+        grace.nama = "Grace";
+        grace.ipk = 90.3;
+        lia.id = 6;
+        lia.nama = "Aprillia";
+        lia.ipk = 92.2;
+
         nasya.print();
+        zahra.print();
+        cinta.print();
+        grace.print();
+        lia.print();
     }
 }
