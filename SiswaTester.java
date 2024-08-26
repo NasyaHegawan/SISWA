644f87ad0cdc30dc8e31e81df1792b9674dec6dd
NasyaHegawan <nasya_putri_33rpl@student.smktelkom-mlg.sch.id>
2024-08-26 13:24:18 +0700

    Add files via upload

--- SiswaTester.java
+++ SiswaTester.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 //Driver Class
 public class SiswaTester {
     public static void main(String[] args) {
+        //Urutannya adalah String - int - double
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nama : ");
+        String nama = input.nextLine();
+        System.out.println("ID : ");
+        int id = input.nextInt();
+        System.out.println("IPK :");
+        double IPK = input.nextDouble();
+        input.close();
+
+        Siswa Dimas = new Siswa(id, nama, IPK);
+        Dimas.print();
+        
         //membuat Object
         //namaClass namaObject = new Constructor ();
         Siswa nasya = new Siswa();
